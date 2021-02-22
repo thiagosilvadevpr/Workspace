e6bfb9651f63dfe504730ad838b5e34152a72e4e
Thiago Silva <thiagosilva.dev.pr@outlook.com>
2021-02-21 22:02:21 -0300

    adicionado classe de conexao

--- src/db/conexao.class.php
+++ src/db/conexao.class.php
@@ -2,10 +2,36 @@
 
 namespace SRC\DB;
 
+use PDO;
+use PDOException;
+
 class Conexao
 {
-    protected $hostName;
-    protected $username;
-    protected $password;
-    protected $db;
+    public $usuario;
+    public $senha;
+    public $banco;
+    public $host;
+    public $driver;
+
+    public function __construct()
+    {
+        $this->usuario = 'root';
+        $this->senha = '';
+        $this->banco = 'test';
+        $this->host = 'host';
+        $this->driver = 'mysql';
+    }
+
+    public function abrirConexao()
+    {
+        try {
+            $conn = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+    }
 }
+
+$conn = new Conexao('root', '');
+$teste->abrirConexao();
